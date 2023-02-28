1338ec16b5ad31227ec10fcf300e496cc742fca8
Soul <mbodapson@gmail.com>
2023-02-28 05:07:30 +0100

    UserController

--- library/src/main/java/com/cassandra/library/repositories/UserRepository.java
+++ library/src/main/java/com/cassandra/library/repositories/UserRepository.java
@@ -9,7 +9,9 @@ import com.cassandra.library.models.User;
 public interface UserRepository extends CassandraRepository<User, String> {
 
     Optional<User> findByEmail(String email);
+    Optional<User> deleteByEmail(String email);
 
     Boolean existsByEmail(String email);
+    List<User> findByNameContaining(String name);
 
 }
